e7d0f4490f0b850b0a31fba4d3939901fdd67ff3
Marlon Velasquez <marlonjvelasquez@gmail.com>
2021-04-06 23:09:58 +0200

    script.js Refactor

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,72 +1,15 @@
 document.addEventListener("click", handlerClick);
 const starList = document.querySelectorAll("i");
-const star1 = 4;
-const star2 = 3;
-const star3 = 2;
-const star4 = 1;
-const star5 = 0;
 const allStar = 5;
 
 function handlerClick(evento) {
   const itemClicked = evento.target.id;
+  const itemNumber = itemClicked.replace(/\D/g, "");
 
   if (itemClicked === "") {
     starsTocheck(allStar);
-  }
-
-  if (itemClicked === "star1") {
-    if (starList[star1].classList.contains("print")) {
-      starList[star1].classList.remove("print");
-    } else {
-      starsToPaint(star1);
-      starList[star1].classList.add("print");
-    }
-
-    starsTocheck(4);
-  }
-
-  if (itemClicked === "star2") {
-    if (starList[star2].classList.contains("print")) {
-      starList[star2].classList.remove("print");
-    } else {
-      starsToPaint(star1, star2);
-    }
-
-    starsTocheck(3);
-  }
-
-  if (itemClicked === "star3") {
-    if (starList[star3].classList.contains("print")) {
-      starList[star3].classList.remove("print");
-    } else {
-      starsToPaint(star1, star2, star3);
-    }
-
-    starsTocheck(2);
-  }
-
-  if (itemClicked === "star4") {
-    if (starList[star4].classList.contains("print")) {
-      starList[star4].classList.remove("print");
-    } else {
-      starsToPaint(star1, star2, star3, star4);
-    }
-
-    starsTocheck(1);
-  }
-
-  if (itemClicked === "star5") {
-    if (starList[star5].classList.contains("print")) {
-      starList[star5].classList.remove("print");
-    } else {
-      starsToPaint(star1, star2, star3, star4, star5);
-    }
-  }
-}
-
-function starsToPaint(...starNumber) {
-  for (const starPosition of starNumber) {
-    starList[starPosition].classList.add("print");
+  } else {
+    classAdder(itemNumber);
   }
 }
 
@@ -75,6 +18,19 @@ function starsTocheck(items) {
     let starItem = starList[index].classList.contains("print");
     if (starItem) {
       starList[index].classList.remove("print");
+    }
+  }
+}
+
+function classAdder(item) {
+  const starItem = starList[item - 1];
+
+  if (starItem.classList.contains("print")) {
+    starsTocheck(item);
+  } else {
+    const nStar = 4;
+    for (let index = item - 1; index <= nStar; index++) {
+      starList[index].classList.add("print");
     }
   }
 }
